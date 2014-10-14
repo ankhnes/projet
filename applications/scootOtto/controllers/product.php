4ca62dc867cb2c40e4fed5a6efc219df9d2caec6
muletJeremy <mulet.jeremy@gmail.com>
2014-10-14 17:08:34 +0200

    Create product
    En cours => Method dans controller product pour l'ajout de produits dans la base.
    Ajout d'une vue "sucess" pour la confirmation que les formulaires sont envoyés

--- applications/scootOtto/controllers/product.php
+++ applications/scootOtto/controllers/product.php
@@ -7,6 +7,22 @@ class Product extends CI_Controller
         $this->load->helper('form');
         $this->load->library('form_validation');
 
+        $this->form_validation->set_rules('model', 'Model', 'trim|xss_clean|required');
+        $this->form_validation->set_rules('description', 'Description', 'trim|xss_clean|required');
+        $this->form_validation->set_rules('price', 'Price', 'trim|xss_clean|required');
+        $this->form_validation->set_rules('specialPrice', 'SpecialPrice', 'trim|xss_clean|required');
+        $this->form_validation->set_rules('availability', 'Availability', 'trim|xss_clean|required');
+        $this->form_validation->set_rules('category', 'Category', 'trim|xss_clean|required');
+        $this->form_validation->set_rules('brand', 'Brand', 'trim|xss_clean|required');
+
+        if ($this->form_validation->run() == FALSE)
+        {
+            $this->load->view('product/create');
+        }
+        else
+        {
+            $this->load->view('success');
+        }
 
     }
 }
---
+++ applications/scootOtto/views/formsuccess.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 14/10/2014
+ * Time: 14:06
+ */ 
