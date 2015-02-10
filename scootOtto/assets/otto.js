d965eaccbfdd054a6f0481c141caae2913959dea
Stobbart <shredder06@gmail.com>
2015-02-11 00:23:35 +0100

    slider ok2

--- scootOtto/assets/otto.js
+++ scootOtto/assets/otto.js
@@ -70,7 +70,7 @@ $(function() {
 $(function() {
 $('.banner').unslider({
 	speed: 500,               //  The speed to animate each slide (in milliseconds)
-	delay: 10000,              //  The delay between slide animations (in milliseconds)
+	delay: 5000,              //  The delay between slide animations (in milliseconds)
 	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
 	dots: true,               //  Display dot navigation
 	fluid: false              //  Support responsive design. May break non-responsive designs
