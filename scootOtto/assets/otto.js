28f1bad38d24e9bcaf26abd2c73e4f7bdbe9dd20
Stobbart <shredder06@gmail.com>
2015-02-11 00:16:51 +0100

    slider ok

--- scootOtto/assets/otto.js
+++ scootOtto/assets/otto.js
@@ -67,5 +67,15 @@ $(function() {
 			});
     });
 
+$(function() {
+$('.banner').unslider({
+	speed: 500,               //  The speed to animate each slide (in milliseconds)
+	delay: 10000,              //  The delay between slide animations (in milliseconds)
+	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+	dots: true,               //  Display dot navigation
+	fluid: false              //  Support responsive design. May break non-responsive designs
+});	
+});
+
 
 });
